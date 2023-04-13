2a10940f65012ede7a65a4e7bcf58f46cf0e9b1b
Peter <pschoenbauer@gmail.com>
2023-04-12 21:27:44 -0400

    find array lab, superbowl Denver Broncos

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // code your solution here
+function superbowlWin(array) {
+    for (const football of array) {
+        if (football.result === "W"){
+            return(football.year)
+        }
+    }
+  }
+
